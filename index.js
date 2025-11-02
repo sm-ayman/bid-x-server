7e9463dc089b9b55927d9c1cf9e7ae3f62259ce4
sm_ayman <sultanmdayman@gmail.com>
2025-11-02 15:13:08 +0600

    Server Created

---
+++ index.js
@@ -0,0 +1,173 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// uri
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ggobuyv.mongodb.net/?appName=Cluster0`;
+
+// mongo-client
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("bid-x server is Running...");
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("bidx_db");
+    const productsCollection = db.collection("products");
+    const bidsCollection = db.collection("bids");
+    const usersCollection = db.collection("users");
+
+    // insert/create/post - product
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productsCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
+    // delete-product
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // update-product
+    app.patch("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedProduct = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: updatedProduct,
+        /*
+          or,
+          name: updatedProduct.name;
+          price: updatedProduct.price;
+        */
+      };
+      const result = await productsCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    // get-all-products
+    app.get("/products", async (req, res) => {
+      // const projectsFields = {title: 1}
+      // const cursor = productsCollection.find().sort({price_min: -1}).limit(5).project(projectsFields);
+
+      console.log(req.query);
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.email = email;
+      }
+
+      const cursor = productsCollection.find(query);
+
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // get-one-product
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // my-bids
+    app.get("/bids", async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.buyer_email = email;
+      }
+
+      const cursor = bidsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // insert-bid
+    app.post("/bids", async (req, res) => {
+      const newBid = req.body;
+      const result = bidsCollection.insertOne(newBid);
+      req.send(result);
+    });
+
+    // delete-bid
+    app.delete("bids/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = bidsCollection.deleteOne(query);
+    });
+
+    // update-bid
+    app.patch("/bids/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedBid = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: updatedBid,
+      };
+      const result = await bidsCollection.updateOne(query, update);
+    });
+
+    // insert-users
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+
+      // existing-user ?
+      const email = req.body.email;
+      const query = { email: email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        res.send({ message: "Ops! User already exists!" });
+      } else {
+        const result = await usersCollection.insertOne(newUser);
+        res.send(result);
+      }
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Smart Server is Running on Port ___ ${port}`);
+});
+
+/* alternative - listen
+
+```
+client.connect().then(() => {
+  app.listen(port, () => {
+    console.log(`Smart Server is Running on Port ___ ${port}`);
+  });
+});
+```
+
+*/
