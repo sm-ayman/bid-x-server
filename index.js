c1e69cfb37181de2230fe110ad3d396781e7419e
sm_ayman <sultanmdayman@gmail.com>
2025-11-03 22:58:17 +0600

    delete bid api updated

--- index.js
+++ index.js
@@ -134,10 +134,11 @@ async function run() {
     });
 
     // delete-bid
-    app.delete("bids/:id", async (req, res) => {
+    app.delete("/bids/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = bidsCollection.deleteOne(query);
+      const result = await bidsCollection.deleteOne(query);
+      res.send(result)
     });
 
     // update-bid
